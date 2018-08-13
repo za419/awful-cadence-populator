d461d5b8e8a1cfc0b6993f8eaf4608dc6a006af4
Ryan Hodin <ryan951357@gmail.com>
2018-08-13 16:11:55 -0700

    Bugfix

--- populate.py
+++ populate.py
@@ -11,10 +11,18 @@ config = config['DEFAULT']
 
 dir = os.path.realpath(sys.argv[2])
 
+timeout=config['db_timeout']
+if timeout=="None":
+    timeout=None
+else:
+    timeout=int(timeout)
+    if timeout<=0:
+        timeout=None
+
 db = pg8000.connect(user=config['db_username'], host=config['db_host'],
                     port=int(config['db_port']), database=config['db_name'],
                     password=config['db_password'], ssl=config.getboolean('db_encrypt'),
-                    timeout=ariaSearch.timeout)
+                    timeout=timeout)
 cursor = db.cursor()
 
 for r, d, files in os.walk(dir):
